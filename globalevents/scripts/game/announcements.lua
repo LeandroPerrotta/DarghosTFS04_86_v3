e491fcb62dd08e6ef2320897493fa2a334be25f1
Leandro Perrotta <leandro_perrotta@hotmail.com>
2012-04-29 07:38:35 +0000

    git-svn-id: svn://darghos.com.br/darghos/tfs/data/trunk@3357 5021ead5-42e4-5646-a3ec-22fc1710fdc7

--- globalevents/scripts/game/announcements.lua
+++ globalevents/scripts/game/announcements.lua
@@ -8,7 +8,7 @@ function autoBroadcast()
 	local messages = {
 		{type = "pvp-channel", text = "Gosta de PvP? Participe de partidas com outros jogadores, ganhe recompensas, conquiste façanhas na Battleground! Guarde seu lugar na fila para a proxima partida com o comando '!bg entrar' agora mesmo!"}
 		--,{text = "Confira os novos preços das Contas Premium! Ter acesso as todas vantagens e beneficios nunca esteve tão barato: www.darghos.com.br"}
-		,{text = "Participe do ultimo evento do Darghos! Ajude o Rei Ordon a recuperar 5 items perdidos e ganhe como recompensa um premium scroll de 30 dias e um item unico! Maiores informações em nosso website! www.darghos.com.br"}
+		--,{text = "Participe do ultimo evento do Darghos! Ajude o Rei Ordon a recuperar 5 items perdidos e ganhe como recompensa um premium scroll de 30 dias e um item unico! Maiores informações em nosso website! www.darghos.com.br"}
 	}
 	
 	local random = math.random(1, #messages)
