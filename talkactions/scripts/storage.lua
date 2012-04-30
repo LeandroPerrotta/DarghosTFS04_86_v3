702ba2caf65da39a86750e096856800bf14d7ec4
Leandro Perrotta <leandro_perrotta@hotmail.com>
2012-04-30 23:11:29 +0000

    git-svn-id: svn://darghos.com.br/darghos/tfs/data/trunk@3366 5021ead5-42e4-5646-a3ec-22fc1710fdc7

--- talkactions/scripts/storage.lua
+++ talkactions/scripts/storage.lua
@@ -5,16 +5,28 @@ function onSay(cid, words, param)
 		return true
 	end
 
-	local tid = getPlayerByNameWildcard(t[1])
-	if(not tid or (isPlayerGhost(tid) and getPlayerGhostAccess(tid) > getPlayerGhostAccess(cid))) then
-		doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "Player " .. t[1] .. " not found.")
-		return true
+	local isGlobal = false
+	
+	if(t[1] ~= "global") then
+		local tid = getPlayerByNameWildcard(t[1])
+		if(not tid or (isPlayerGhost(tid) and getPlayerGhostAccess(tid) > getPlayerGhostAccess(cid))) then
+			doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "Player " .. t[1] .. " not found.")
+			return true
+		end
+	else
+		isGlobal = true
 	end
 
 	if(not t[3]) then
-		doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, " [" .. t[1] .. " - " .. t[2] .. "] = " .. getPlayerStorageValue(tid, t[2]))
+		
+		local value = isGlobal and getPlayerStorageValue(t[2]) or getPlayerStorageValue(tid, t[2])
+		doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, " [" .. t[1] .. " - " .. t[2] .. "] = " .. value)
 	else
-		setPlayerStorageValue(tid, t[2], t[3])
+		if(isGlobal) then
+			setGlobalStorageValue(t[2], t[3])
+		else
+			setPlayerStorageValue(tid, t[2], t[3])
+		end
 	end
 
 	return true
