aaf089417adcba120451506db9f19967d17eb099
Leandro Perrotta <leandro_perrotta@hotmail.com>
2012-05-11 22:14:56 +0000

    git-svn-id: svn://darghos.com.br/darghos/tfs/data/trunk@3389 5021ead5-42e4-5646-a3ec-22fc1710fdc7

--- creaturescripts/scripts/game/logout.lua
+++ creaturescripts/scripts/game/logout.lua
@@ -8,20 +8,22 @@ function onLogout(cid, forceLogout)
 	local onLoginLevel = tonumber(getPlayerStorageValue(cid, sid.LOGIN_LEVEL))
 	local onLoginExperience = tonumber(getPlayerStorageValue(cid, sid.LOGIN_EXPERIENCE))
 	
-	local queryStr = "INSERT INTO `player_activities` VALUES ("
+	if(onLoginLevel ~= -1 and onLoginExperience ~= -1) then
+		local queryStr = "INSERT INTO `player_activities` VALUES ("
+			
+		queryStr = queryStr .. getPlayerGUID(cid)
+		queryStr = queryStr .. ", " .. getPlayerLastLogin(cid)
+		queryStr = queryStr .. ", " .. os.time() - getPlayerLastLogin(cid)
+		queryStr = queryStr .. ", " .. onLoginExperience
+		queryStr = queryStr .. ", " .. getPlayerExperience(cid)
+		queryStr = queryStr .. ", " .. onLoginLevel
+		queryStr = queryStr .. ", " .. getPlayerLevel(cid)
+		queryStr = queryStr .. ", " .. getPlayerIp(cid)
 		
-	queryStr = queryStr .. getPlayerGUID(cid)
-	queryStr = queryStr .. ", " .. getPlayerLastLogin(cid)
-	queryStr = queryStr .. ", " .. os.time() - getPlayerLastLogin(cid)
-	queryStr = queryStr .. ", " .. onLoginExperience
-	queryStr = queryStr .. ", " .. getPlayerExperience(cid)
-	queryStr = queryStr .. ", " .. onLoginLevel
-	queryStr = queryStr .. ", " .. getPlayerLevel(cid)
-	queryStr = queryStr .. ", " .. getPlayerIp(cid)
-	
-	queryStr = queryStr .. ");"
-	
-	db.executeQuery(queryStr)
+		queryStr = queryStr .. ");"
+		
+		db.executeQuery(queryStr)
+	end
 	
 	return true
 end 
