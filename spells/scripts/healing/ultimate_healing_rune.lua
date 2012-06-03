2e18507f8c8b67f6484557067722c9eb26a53b6c
Leandro Perrotta <leandro_perrotta@hotmail.com>
2012-06-03 05:56:55 +0000

    git-svn-id: svn://darghos.com.br/darghos/tfs/data/trunk@3461 5021ead5-42e4-5646-a3ec-22fc1710fdc7

--- spells/scripts/healing/ultimate_healing_rune.lua
+++ spells/scripts/healing/ultimate_healing_rune.lua
@@ -7,6 +7,12 @@ setCombatParam(combat, COMBAT_PARAM_DISPEL, CONDITION_PARALYZE)
 
 function onGetFormulaValues(cid, level, maglevel)
 	local min, max = getMinMaxClassicFormula(level, maglevel, 11.2, 14.5, 250, 300)
+
+	if(isPaladin(cid)) then
+		min = math.ceil(min * 0.80)
+		max = math.ceil(max * 0.80)
+	end	
+	
 	return min, max
 end
 
@@ -24,11 +30,6 @@ function onCastSpell(cid, var)
 		doPlayerSendCancel(cid, "Somente druids podem usar runas de regeneração em outros jogadores.")
 		doSendMagicEffect(getPlayerPosition(cid), CONST_ME_POFF)
 		return false		
-	end
-	
-	if(isPlayer(target) and isPaladin(target)) then
-		min = math.ceil(min * 0.80)
-		max = math.ceil(max * 0.80)	
 	end
 	
 	return doCombat(cid, combat, var)
